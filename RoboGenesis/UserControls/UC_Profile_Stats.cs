705c57baf44d06143d5ae09c112e7860fd5eddea
aksinyalk-yukhno <147872730+aksinyalk-yukhno@users.noreply.github.com>
2024-10-06 18:09:11 +0300

    Update UC_Profile_Stats.cs

--- RoboGenesis/UserControls/UC_Profile_Stats.cs
+++ RoboGenesis/UserControls/UC_Profile_Stats.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OutlookDemo.UserControls
+namespace RoboGenesis.UserControls
 {
     public partial class UC_Profile_Stats : UserControl
     {
