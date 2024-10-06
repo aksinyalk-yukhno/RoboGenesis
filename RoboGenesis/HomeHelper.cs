8fabb1fc7494cdbcf62b917397bfaab9fa47d798
aksinyalk-yukhno <147872730+aksinyalk-yukhno@users.noreply.github.com>
2024-10-06 18:49:26 +0300

    Update HomeHelper.cs

--- RoboGenesis/HomeHelper.cs
+++ RoboGenesis/HomeHelper.cs
@@ -5,9 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static OutlookDemo.HomeHelper;
+using static RoboGenesis.HomeHelper;
 
-namespace OutlookDemo
+namespace RoboGenesis
 {
     internal class HomeHelper
     {
