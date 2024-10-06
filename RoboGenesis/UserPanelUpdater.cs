dad4e6648d6cc5eff46c8af04fc54d6f09f99298
aksinyalk-yukhno <147872730+aksinyalk-yukhno@users.noreply.github.com>
2024-10-06 18:52:49 +0300

    Update UserPanelUpdater.cs

--- RoboGenesis/UserPanelUpdater.cs
+++ RoboGenesis/UserPanelUpdater.cs
@@ -1,4 +1,4 @@
-﻿using OutlookDemo.UserControls;
+﻿using RoboGenesis.UserControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OutlookDemo
+namespace RoboGenesis
 {
     internal class UserPanelUpdater
     {
