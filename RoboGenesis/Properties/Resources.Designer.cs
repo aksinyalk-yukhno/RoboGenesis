73bb9e0fc2c82cf601464b1dad9c49c408ece962
aksinyalk-yukhno <147872730+aksinyalk-yukhno@users.noreply.github.com>
2024-10-06 18:18:48 +0300

    Update Resources.Designer.cs

--- RoboGenesis/Properties/Resources.Designer.cs
+++ RoboGenesis/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace OutlookDemo.Properties {
+namespace RoboGenesis.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace OutlookDemo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("OutlookDemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RoboGenesis.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
