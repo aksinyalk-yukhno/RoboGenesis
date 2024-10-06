668fc40202ee35b2baff33dd54b014a23ccaf871
aksinyalk-yukhno <147872730+aksinyalk-yukhno@users.noreply.github.com>
2024-10-06 17:59:42 +0300

    Update UC_Home_Category.cs

--- RoboGenesis/UserControls/UC_Home_Category.cs
+++ RoboGenesis/UserControls/UC_Home_Category.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OutlookDemo.UserControls
+namespace RoboGenesis.UserControls
 {
     public partial class UC_Home_Category : UserControl
     {
